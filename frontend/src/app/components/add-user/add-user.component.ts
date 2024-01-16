950f605e9d0ed41ec88239f8ac0ae423c79b08c9
R-Grigala <r.grigalashvili777@gmail.com>
2024-01-16 15:42:45 +0400

    Coding Grigala

--- frontend/src/app/components/add-user/add-user.component.ts
+++ frontend/src/app/components/add-user/add-user.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { UsersService } from '../../users.service';
+import { Router } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-user',
@@ -8,5 +11,20 @@ import { Component } from '@angular/core';
   styleUrl: './add-user.component.css'
 })
 export class AddUserComponent {
+  constructor(private userService: UsersService, private router: Router){}
 
+  data: any
+
+  form = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    gender: new FormControl('', Validators.required)
+  })
+
+  addUser(){
+    this.data = this.form.value
+    this.userService.addUser(this.data).subscribe(data => {
+      this.router.navigate(['/']) // redirect to homepage
+    })
+  }
 }
--- frontend/src/app/components/update-user/update-user.component.ts
+++ frontend/src/app/components/update-user/update-user.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from 'src/app/app.service';
+import { User } from 'src/app/User';
 
 @Component({
   selector: 'app-update-user',
-  standalone: true,
-  imports: [],
   templateUrl: './update-user.component.html',
-  styleUrl: './update-user.component.css'
+  styleUrls: ['./update-user.component.css']
 })
 export class UpdateUserComponent {
+
+  user?: any
+  data: any
+
+
+  constructor(private service: UserService, private route: ActivatedRoute, private router : Router) { }
+
+  ngOnInit(): void {
+    let id = this.route.snapshot.params['id'];
+    this.service.getUser(id).subscribe(data => {
+      this.user = data
+      console.log(this.user)
+    })
+  }
+
+  form = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required]),
+  })
+
+  submit(){
+    this.data = this.form.value
+    this.user.name = this.data.name
+    this.user.email = this.data.email
+    console.log(this.data)
+    
+    this.service.updateUser(this.user?.id, this.user).subscribe(data => {
+      console.log(data)
+    })
+
+    this.router.navigate(['/']);
+  }
 
 }
